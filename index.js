72a0615f2074657bf787155168e360986f44fd9a
essential-form-0157 <vasmith430@gmail.com>
2020-07-17 15:28:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeliLine = [ ];
+
+var takeANumber = (katzDeliLine, "Ada");
+takeANumber (katzDeliLine, "Grace");
+takeANumber (katzDeliLine, "Kent");
+
+currentLine (katzDeliLine);
+
+nowServing (katzDeliLine);
